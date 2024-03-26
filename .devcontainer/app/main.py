56b479b06e23361b744c6d9729788e87f4f25ac2
Antek Wnorowski <vivaldix.96@gmail.com>
2024-03-26 03:24:17 +0000

    models can be trained on the JSON - for example with data generated by the GET training_data endpoint

--- .devcontainer/app/main.py
+++ .devcontainer/app/main.py
@@ -1,12 +1,17 @@
 import asyncio
 from flask import Flask, jsonify
 from fastapi import FastAPI
-from services import train_model
+from services import dummy_function, train_models, generate_data, dump_models
 from schemas import NumbersInput
+from models import MachineLearningData
+from typing import List
+
 
 from time import sleep   ### for testing on dummy tasks
 
 app = FastAPI()
+
+global TASK_CORO # declaring a variable for monitoring of current training status
 
 @app.get("/")
 async def root():
@@ -14,16 +19,10 @@ async def root():
 
 
 
-# endpoint for initiation of the model training
+@app.get("/training_data")
+async def generate_training_data():
+    return await generate_data()
 
-#app. ...
-#async def ...
-
-
-
-# endpoint for checking the training status
-#app.get
-#
 
 """
 app = Flask(__name__)
@@ -37,29 +36,32 @@ if __name__ == "__main__":
   app.run()
 
   """
-@app.post("/input_array/")
+@app.post("/input_array")
 async def validate_numbers(numbers_input: NumbersInput):
     return{"message":"Correct input received. Proceeding."}
 
-async def dummy_function():
-    print("Dummy function started")
-    await asyncio.sleep(20)
-    print("Dummy function finished")
+# async def dummy_function():
+#     print("Dummy function started")
+#     await asyncio.sleep(20)
+#     print("Dummy function finished")
 
-async def check_function_status():
-    if 'task_coro' in globals() and not task_coro.done():
-        return "Function is running"
-    else:
-        return "Function is not running"
 
-@app.get("/task_status")
-async def get_task_status():
-    return await check_function_status()
+# endpoint for initiation of the model training on the data delivered 
+# by the User - preferably, copied after generating from the training_data endpoint
+@app.post("/train-models")
+async def train_models_on_JSON(datasets_batch: list[MachineLearningData]):
+    
+    trained_models = await train_models(datasets_batch)
+    dump_models(trained_models)
+    #return {"trained_models": trained_models, "training_success": training_success} ###### CHANGE - only dump the models, not try to return them
+
+
+
 
 @app.get("/start_task")
 async def start_task():
-    global task_coro
-    task_coro = asyncio.create_task(dummy_function())
+     #
+    TASK_CORO = asyncio.create_task(dummy_function())
     return "Task started"
 
 async def dummy_machine_learning_task():
@@ -72,16 +74,16 @@ async def dummy_machine_learning_task():
         return None  # Return None to indicate failure
 
 async def check_task_status():
-    if 'task_coro' in globals() and not task_coro.done():
-        return "Task is running"
-    elif 'task_coro' in globals() and task_coro.done():
-        result = await task_coro
+    if 'TASK_CORO' in globals() and not TASK_CORO.done():
+        return "Training in progress..."
+    elif 'TASK_CORO' in globals() and TASK_CORO.done():
+        result = await TASK_CORO
         if result:
-            return "Task finished successfully"
+            return "Model training finished successfully"
         else:
-            return "Task finished but failed"
+            return "Model training finished but failed"
     else:
-        return "No task running"
+        return "No model is being currently trained"
 
 @app.get("/task_status")
 async def get_task_status():
@@ -89,7 +91,9 @@ async def get_task_status():
 
 @app.get("/start_task")
 async def start_task():
-    global task_coro
-    task_coro = asyncio.create_task(dummy_machine_learning_task())
+    global TASK_CORO
+    TASK_CORO = asyncio.create_task(dummy_machine_learning_task())
     return "Task started"
  
+# @app.delete("/task-disruptor")
+# async def end_task_with_a_failure(): 
